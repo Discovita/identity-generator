633cf5d9caa9458406e6f0e5ac3e953979a0709c
Casey Schmid <caseywschmid@gmail.com>
2025-04-20 13:07:12 +0200

    added new-coach to the run_project script

--- scripts/utils/app_selector.sh
+++ scripts/utils/app_selector.sh
@@ -4,6 +4,7 @@ get_app_name() {
     echo "Which app would you like to run?"
     echo "1) face-swapper"
     echo "2) coach"
+    echo "3) new-coach"
     echo
-    echo -n "Enter choice (1-2): "
+    echo -n "Enter choice (1-3): "
 }
